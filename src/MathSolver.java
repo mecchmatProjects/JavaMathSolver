4ba3c9eefbac91127febf0db2d1c9b60406dc5f6
kiril <kirabatrak@gmail.com>
2026-09-10 14:10:33 +0300

    feat(core): setup MathSolver skeleton and detect args[0]

---
+++ src/MathSolver.java
@@ -0,0 +1,14 @@
+public class MathSolver {
+    public static void main(String[] args) {
+        // Базовий захист від виклику без параметрів
+        if (args.length == 0) {
+            System.out.println("JavaMathSolver v0.1");
+            System.out.println("Use 'help' to see available commands.");
+            return;
+        }
+
+        // CORE-01: Визначення назви операції з args[0]
+        String command = args[0];
+        System.out.println("Command received: " + command);
+    }
+}
