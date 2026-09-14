5821fc98959bfe3ad41e85c219683c3e302f25e4
YevhenPoshtak <yevhenposhtak13@gmail.com>
2026-09-14 19:01:06 +0300

    feat(geometry): add MathSolver base, GEO-01, GEO-02

---
+++ src/MathSolver.java
@@ -0,0 +1,28 @@
+public class MathSolver {
+    public static void main(String[] args) {
+
+    }
+
+    // GEO-01: Distance between points
+    public static void calculateDistance(String[] args) {
+        double x1 = Double.parseDouble(args[1]);
+        double y1 = Double.parseDouble(args[2]);
+        double x2 = Double.parseDouble(args[3]);
+        double y2 = Double.parseDouble(args[4]);
+
+        double d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("distance = " + d);
+    }
+
+    // GEO-02: Circle area
+    public static void calculateCircleArea(String[] args) {
+        double r = Double.parseDouble(args[1]);
+        if (r < 0) {
+            System.out.println("Error: radius cannot be negative");
+            return;
+        }
+        double s = Math.PI * r * r;
+        System.out.println("circle area = " + s);
+    }
+
+}
