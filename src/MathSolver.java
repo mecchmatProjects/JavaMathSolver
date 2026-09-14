558946faf80cbfae4caa6bc36e3d8a4d6f8a8ce3
margo <tmilkamargo@gmail.com>
2026-09-14 21:19:14 +0300

    feat(core): implement help command

--- src/MathSolver.java
+++ src/MathSolver.java
@@ -9,6 +9,29 @@ public class MathSolver {
 
         // CORE-01: Визначення назви операції з args[0]
         String command = args[0];
+
+        // CORE-04: Обробка команди help
+        if (command.equals("help")) {
+            printHelp();
+            return;
+        }
+
         System.out.println("Command received: " + command);
+    }
+
+    public static void printHelp() {
+        System.out.println("JavaMathSolver\n");
+        System.out.println("Available commands:\n");
+        System.out.println("add a b");
+        System.out.println("sub a b");
+        System.out.println("mul a b");
+        System.out.println("div a b");
+        System.out.println("pow a b");
+        System.out.println("sqrt x\n");
+        System.out.println("distance x1 y1 x2 y2");
+        System.out.println("circle-area r");
+        System.out.println("circle-circumference r");
+        System.out.println("rectangle-area a b");
+        System.out.println("origin-distance x y");
     }
 }
