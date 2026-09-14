a8cde8ebe00e47c383c906351a0850039edd2780
Platon-Pavlenko <pavlenkoplaton@knu.ua>
2026-09-14 12:32:13 +0300

    testing

--- src/MathSolver.java
+++ src/MathSolver.java
@@ -10,5 +10,44 @@ public class MathSolver {
         // CORE-01: Визначення назви операції з args[0]
         String command = args[0];
         System.out.println("Command received: " + command);
+
+        // CORE-02 перевірка потрібної кількості аргументів
+        if(command.equals("add") || command.equals("sub") || command.equals("mul") || command.equals("div") || command.equals("pow")
+                || command.equals("origin-distance") || command.equals("rectangle-area") || command.equals("rectangle-perimeter")){
+            if(args.length<3) {
+                System.out.println("Error: Not enough arguments");
+                System.out.println("Use one more number!");
+                return;
+            }
+            else if(args.length>3){
+                    System.out.println("Error: Too many arguments");
+                    System.out.println("Use only 2 numbers!");
+                    return;
+            }
+        }
+        if(command.equals("abs") || command.equals("sqrt") || command.equals("circle-area") || command.equals("circle-circumference")){
+            if(args.length<2){
+                System.out.println("Error: Too many arguments");
+                System.out.println("Use a number!");
+                return;
+            }
+            else if(args.length>2){
+                System.out.println("Error: Too many arguments");
+                System.out.println("Use only one number!");
+                return;
+            }
+        }
+        if(command.equals("distance")){
+            if(args.length<5) {
+                System.out.println("Error: Not enough arguments");
+                System.out.println("Use 4 numbers!");
+                return;
+            }
+            else if(args.length>5){
+                System.out.println("Error: Too many arguments");
+                System.out.println("Use only 4 numbers!");
+                return;
+            }
+        }
     }
 }
